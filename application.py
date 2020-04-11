aa8c7f88e6240baa5f2c3eb9e5271bd3ea5b07c0
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-04-11 18:12:56 -0400

    Add further conditional blocks

--- application.py
+++ application.py
@@ -7,6 +7,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yesOrNo = input("Did you mean %s instead? Enter Y if Yes, or N if No: " % get_close_matches(word, data.keys())[0])
         if yesOrNo == "Y":
